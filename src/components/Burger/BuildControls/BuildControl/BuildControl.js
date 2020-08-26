5e78c260b8db7f41455fe4dd4daac8944b6ca9f0
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-08-27 01:39:11 +0200

    Add ingredients

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import classes from './BuildControl.module.css'
 
-const BuildControl = ({ label }) => (
+const BuildControl = ({ label, added }) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{label}</div>
     <button className={classes.Less}>Less</button>
-    <button className={classes.More}>More</button>
+    <button className={classes.More} onClick={added}>
+      More
+    </button>
   </div>
 )
 
